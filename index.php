8db7a059d783e1d2b10e4bf9d4c3c633698281a3
David Ebert <ebertek@gmail.com>
2017-07-06 19:00:05 +0200

    Add CV

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <?php
   $useragent = $_SERVER['HTTP_USER_AGENT'];
   $os        = array("default", "Macintosh", "iPhone", "iPad", "iPod", "Android");
-  $chosen    = 0;
+  $device    = 0;
   $tombcount = count($os);
   for ($i = 0; $i < $tombcount; $i++) {
     if (preg_match("/$os[$i]/", $useragent)) {
-      $chosen = $i;
+      $device = $i;
       break;
     }
   }
-  if (2 <= $chosen && $chosen <=4) {
-    $chosen = 2;
+  if (2 <= $device && $device <=4) {
+    $device = 2;
   }
   /**
    * 0: default
@@ -19,8 +19,10 @@
    * 3: Android
    */
   $message = array("mailto", "imessage", "sms", "mailto");
-  $call = array("callto", "tel", "tel", "tel");
-  $fsq = array("https://foursquare.com/ebertek", "https://foursquare.com/ebertek", "foursquare://users/200263", "https://foursquare.com/ebertek");
+  $call    = array("callto", "tel", "tel", "tel");
+  $fsq     = array("https://foursquare.com/ebertek", "https://foursquare.com/ebertek", "foursquare://users/200263", "https://foursquare.com/ebertek");
+
+  $country_code = $_SERVER["HTTP_CF_IPCOUNTRY"]; // https://support.cloudflare.com/hc/en-us/articles/200168236-What-does-Cloudflare-IP-Geolocation-do-
 ?>
 <!DOCTYPE html>
 
@@ -30,16 +32,16 @@
     <meta charset="UTF-8" />
     <meta http-equiv="x-ua-compatible" content="ie=edge" />
     <title>ebertek.com</title>
-    <meta name="Description" content="David Ebert is an introverted college dropout with Apple certifications and a Netflix addiction." />
+    <meta name="Description" content="David Ebert is a taciturn ISTJ; a perpetual student @ BUTE; a sysadmin with some Apple certifications; a Netflix addict." />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-    <meta name="robots" content="index, follow" />
+    <meta name="robots"    content="index, follow" />
     <meta name="googlebot" content="index, follow" />
 
     <base href="https://ebertek.com/" />
     <meta name="url" content="https://ebertek.com/" />
 
-    <link href="humans.txt" rel="author" type="text/plain" />
+    <link href="humans.txt"   rel="author"     type="text/plain" />
     <link href="css/main.css" rel="stylesheet" type="text/css" />
   </head>
 
@@ -55,19 +57,20 @@
         <p>
           <a href="twitter:@ebertek" title="Twitter">Twitter</a><br />
           <a href="fb://profile/1151623436" title="Facebook">Facebook</a><br />
-          <a href="<?php echo $message[$chosen]; ?>:ebertek@mac.com" title="Message">ebertek@mac.com</a><br />
-          <a href="<?php echo $call[$chosen]; ?>:+36302750065" title="Call">+36 (30) 275 0065</a><br />
+          <a href="<?php echo $message[$device]; ?>:ebertek@mac.com" title="Message">ebertek@mac.com</a><br />
+          <a href="<?php echo $call[$device]; ?>:+36302750065" title="Call">+36 (30) 275 0065</a><br />
         </p>
         <p>
+          <?php if ($country_code == "HU") echo '<a href="cv.pdf" title="Curriculum Vitæ">CV / Resume (🇭🇺)</a><br />'; ?>
           <a href="https://www.linkedin.com/in/ebertdavid" title="LinkedIn">LinkedIn</a><br />
           <a href="https://github.com/ebertek" title="GitHub">GitHub</a><br />
           <a href="http://www.last.fm/user/ebertek" title="Last.fm">Last.fm</a><br />
           <a href="https://trakt.tv/users/ebertek" title="trakt.tv">trakt.tv</a><br />
-          <a href="<?php echo $fsq[$chosen]; ?>" title="foursquare">foursquare</a><br />
+          <a href="<?php echo $fsq[$device]; ?>" title="foursquare">foursquare</a><br />
           <a href="https://www.flickr.com/photos/ebertek" title="flickr">flickr</a><br />
         </p>
         <p>
-          <?php if ($chosen==1 || $chosen==2) echo '<a href="facetime:ebertek@mac.com" title="FaceTime">FaceTime</a><br />'; ?>
+          <?php if ($device == 1 || $device == 2) echo '<a href="facetime:ebertek@mac.com" title="FaceTime">FaceTime</a><br />'; ?>
         </p>
       </div>
 

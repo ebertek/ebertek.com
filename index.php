5da91174406b11c05b1abcc92be471c356a87a5e
David Ebert <ebertek@gmail.com>
2024-03-25 16:38:18 +0100

    Simplify homepage

--- index.php
+++ index.php
@@ -1,28 +1,3 @@
-<?php
-  $useragent = $_SERVER['HTTP_USER_AGENT'];
-  $os        = array("default", "Macintosh", "iPhone", "iPad", "iPod", "Android");
-  $device    = 0;
-  $tombcount = count($os);
-  for ($i = 0; $i < $tombcount; $i++) {
-    if (preg_match("/$os[$i]/", $useragent)) {
-      $device = $i;
-      break;
-    }
-  }
-  if (2 <= $device && $device <=4) {
-    $device = 2;
-  }
-  /**
-   * 0: default
-   * 1: macOS
-   * 2: iOS: iPhone, iPad, iPod
-   * 3: Android
-   */
-  $twttr   = array("https://twitter.com/ebertek", "twitter:@ebertek", "twitter:@ebertek", "twitter:@ebertek");
-  $fb      = array("https://www.facebook.com/david.ebert", "fb://profile/1151623436", "fb://profile/1151623436", "fb://profile/1151623436");
-  $message = array("mailto", "imessage", "sms", "mailto");
-  $call    = array("callto", "tel", "tel", "tel");
-?>
 <!DOCTYPE html>
 
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
@@ -51,21 +26,16 @@
 
       <main>
         <ul>
-          <li><a href="<?php echo $twttr[$device]; ?>" title="Twitter">Twitter</a></li>
-          <li><a href="<?php echo $fb[$device]; ?>" title="Facebook">Facebook</a></li>
-          <li><a href="<?php echo $message[$device]; ?>:ebertek@mac.com" title="Message">ebertek<wbr />@mac.com</a></li>
-          <li><a href="<?php echo $call[$device]; ?>:+46735519258" title="Call">+46 73-551 92 58</a></li>
-<?php if ($device == 1 || $device == 2) echo '          <li><a href="facetime:ebertek@mac.com" title="FaceTime">FaceTime</a></li>' . PHP_EOL; ?>
+          <li><a href="https://www.messenger.com/t/david.ebert" title="Facebook Messenger">Messenger</a></li>
+          <li><a href="mailto:ebertek@gmail.com" title="Email">ebertek<wbr />@gmail.com</a></li>
         </ul>
         <ul>
-          <li><a href="cv-david_ebert.pdf" title="Curriculum Vitæ" type="application/pdf">CV / Résumé</a></li>
           <li><a href="https://www.linkedin.com/in/ebertdavid/" title="LinkedIn">LinkedIn</a></li>
-          <li><a href="https://github.com/ebertek" title="GitHub">GitHub</a></li>
-          <li><a href="https://www.instagram.com/ebertek/" title="Instagram">Instagram</a></li>
-          <li><a href="http://www.last.fm/user/ebertek" title="Last.fm">Last.fm</a></li>
-          <li><a href="https://trakt.tv/users/ebertek" title="trakt.tv">trakt.tv</a></li>
-          <li><a href="https://foursquare.com/ebertek" title="foursquare">foursquare</a></li>
-          <li><a href="https://www.flickr.com/photos/ebertek" title="flickr">flickr</a></li>
+          <li><a href="https://github.com/ebertek"              title="GitHub">GitHub</a></li>
+          <li><a href="https://www.instagram.com/ebertek/"      title="Instagram">Instagram</a></li>
+          <li><a href="http://www.last.fm/user/ebertek"         title="Last.fm">Last.fm</a></li>
+          <li><a href="https://trakt.tv/users/ebertek"          title="trakt.tv">trakt.tv</a></li>
+          <li><a href="https://foursquare.com/ebertek"          title="foursquare">foursquare</a></li>
         </ul>
         <p>&nbsp;</p>
         <ul>
